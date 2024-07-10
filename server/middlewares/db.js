4bc472ebd8fc8801526853af10b5ecfc67992d03
Al Faiz Ali <alfaizali8410@gmail.com>
2024-07-11 01:17:04 +0530

    Added SigIn/SignUp form

--- server/middlewares/db.js
+++ server/middlewares/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-const mongodburi = process.env.MONGODB_URI 
+// const mongodburi = process.env.MONGODB_URI 
 // || 'mongodb://127.0.0.1:27017/Gamic'
-export default await mongoose.connect(mongodburi)
+export default await mongoose.connect('mongodb+srv://AlFaizAli:cg3m9hcGWuKDOOZn@cluster0.smu2cu7.mongodb.net/Gamic')
     .then(
         () => { console.log("db connected") }
     )
